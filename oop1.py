289c9bb92d85db35a7c152e48292d3551e501e61
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-02-10 14:47:11 +0530

    Create oop1.py

---
+++ oop1.py
@@ -0,0 +1,12 @@
+#class created by class key word
+class Vivomobile:
+    #properties =variables
+    #methods
+    x=99  #properity
+    def samplemethode(self):   #methods functions
+        print(self.x)
+#we create one object
+obj1=Vivomobile()
+obj1.samplemethode()
+Vivomobile.samplemethode(obj1)
+
