25a0ef16ae2465238d12443a326e0b86c2d21161
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-02-10 15:52:35 +0530

    Create multiple_inhertence.py

---
+++ multiple_inhertence.py
@@ -0,0 +1,15 @@
+class college:
+    def one(self):
+        print("this is iit roorkree")
+class eee(college):
+    def two(self):
+        print("we are studying here")
+class sc(eee):
+    def three(self):
+        print("my cource is syayems and control")
+
+#create object
+obj=sc()
+obj.one()
+obj.two()
+obj.three()
