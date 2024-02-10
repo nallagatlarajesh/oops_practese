aa21eb5fc1ac3300e8f1e6ac8d8c80032e2e3c5f
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-02-10 14:48:35 +0530

    Create init.py

---
+++ init.py
@@ -0,0 +1,20 @@
+class VivoMobile:
+    def __init__(self):
+        print("welcome to Vivo mobiles")
+
+    def info(self):
+        print("x50","16gb","256gb")
+
+    def example(self):
+        print("example is working....")
+#we create objects
+mob1=VivoMobile()
+
+#accesing the attributes
+mob1.info()
+
+mob2=VivoMobile()
+mob2.info()
+
+#accesing the attributes
+#mob1.example()
